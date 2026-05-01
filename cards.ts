1d7eae26839ea19eddce5e5c558416c4fc4df2b6
b <brt.ozgr@hotmail.com>
2026-05-01 10:20:24 +0200

    Update cards.ts

--- cards.ts
+++ cards.ts
@@ -1,3 +1,38 @@
-export function hello(): string {
-  return "world";
+export enum Farben {
+    Rosen,
+    Schellen,
+    Schilten,
+    Eicheln
+}
+
+export enum Werte {
+    Ass,
+    Koenig,
+    Ober,
+    Under,
+    Banner,
+    Neun,
+    Acht,
+    Sieben,
+    Sechs
+
+}
+
+
+export class Card {
+    constructor(public farben: Farben, public werte: Werte) { }
+
+    beats(other: Card): boolean {
+        if (this.werte < other.werte) return true;
+        if (this.werte > other.werte) return false;
+        if (this.werte == other.werte) return this.vergleichFarben(this.farben, other.farben);
+
+
+    }
+
+    private vergleichFarben(a: Farben, b: Farben): boolean {
+        if (a == b) return false;
+    }
+
+
 }
